4185b0dbf284da851b2e1e710b4bbe804d214e76
Ivan Monastyrev <ikloster@yandex.ru>
2020-02-11 00:00:30 +0300

    main: comment rtdbPlugin

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// import { rtdbPlugin } from 'vuefire'
 
+// Vue.use(rtdbPlugin)
 Vue.config.productionTip = false
 
 new Vue({
