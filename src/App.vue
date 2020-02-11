ac21e91e9428865cf722cdfc26d6e8d561a54572
Ivan Monastyrev <ikloster@yandex.ru>
2020-02-11 22:12:47 +0300

    app: add global styles

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,18 @@
 </template>
 
 <script>
+import { injectGlobal } from 'vue-styled-components'
+
+injectGlobal`
+  #app, #app * {
+    box-sizing: border-box;
+  }
+	body {
+		margin: 0;
+    padding: 0;
+	}
+`
+
 export default {
   name: 'App',
 }
