376a1d73084114623d8aa06d068226d2820415cf
Ivan Monastyrev <ikloster@yandex.ru>
2020-02-11 23:41:19 +0300

    pages: add habit (calendar)

--- src/views/Habit.vue
+++ src/views/Habit.vue
@@ -1,10 +1,53 @@
 <template>
-  <div></div>
+  <main>
+    <page-title>Habit: {{ title }}</page-title>
+    <wrapper>
+      <date-picker mode="multiple" v-model="dates" color="blue" is-inline />
+    </wrapper>
+  </main>
 </template>
 
 <script>
+import { mapState, mapGetters } from 'vuex'
+import moment from 'moment'
+// import Calendar from 'v-calendar/lib/components/calendar.umd'
+import DatePicker from 'v-calendar/lib/components/date-picker.umd'
+import PageTitle from '@/components/PageTitle'
+import Wrapper from '@/components/Wrapper'
+
 export default {
   name: 'Habit',
+  components: {
+    'page-title': PageTitle,
+    Wrapper,
+    DatePicker,
+  },
+  computed: {
+    ...mapState(['habitHistories']),
+    ...mapGetters(['habit', 'histories']),
+    title() {
+      const habitId = parseInt(this.$route.params.habitId)
+      const habit = this.habit(habitId)
+
+      return habit ? habit.title : ''
+    },
+  },
+  methods: {
+    historiesByHabit() {
+      const habitId = parseInt(this.$route.params.habitId)
+      const habitHistories = this.histories(habitId)
+
+      return habitHistories.map(h => moment(h).toDate())
+    },
+  },
+  data() {
+    return {
+      dates: [],
+    }
+  },
+  mounted() {
+    this.dates = this.historiesByHabit()
+  }
 }
 </script>
 
