4c407b774dd6a1ddf93b94f0b01f2e2c9163f4a2
Ivan Monastyrev <ikloster@yandex.ru>
2020-02-12 23:32:39 +0300

    habit:  add text input

---
+++ src/components/TextInput.js
@@ -0,0 +1,10 @@
+import styled from 'vue-styled-components'
+
+const TextInput = styled.input`
+  display: block;
+  font-size: 14px;
+  color: #111;
+  padding: 0 16px;
+  border: 1px solid #111;
+`
+export default TextInput
