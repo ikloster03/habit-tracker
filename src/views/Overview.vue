c0418752a2f797b237df3d6f86370f62f83882df
Ivan Monastyrev <ikloster@yandex.ru>
2020-02-14 00:18:58 +0300

    overview: add sidebar

--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -1,34 +1,39 @@
 <template>
-  <main>
-    <page-title>Overview</page-title>
-    <overview>
-      <overview-row>
-        <overview-column>
-          <overview-title>Habits</overview-title>
-        </overview-column>
-        <overview-column
-          v-for="(day, col) in getLastNDays(FIVE_DAYS)"
-          :key="`habit-col-${col}`"
-        >
-          <overview-title>{{ day | formatDay }}</overview-title>
-        </overview-column>
-      </overview-row>
-      <overview-row v-for="habit in habits" :key="`habit-${habit.id}`">
-        <overview-column>
-          <router-link :to="{ name: 'habit', params: { habitId: habit.id } }">
-            {{ habit.title }}
-          </router-link>
-        </overview-column>
-        <overview-column
-          v-for="(day, col) in getLastNDays(FIVE_DAYS)"
-          :key="`habit-col-${col}`"
-        >
-          <div v-if="isHabitMark(habit.id, formatDate(day))">x</div>
-        </overview-column>
-      </overview-row>
-    </overview>
-    <add-habbit-button @click.prevent.native="openModal()">+</add-habbit-button>
-  </main>
+  <div>
+    <sidebar />
+    <main id="page-wrap">
+      <page-title>Overview</page-title>
+      <overview>
+        <overview-row>
+          <overview-column>
+            <overview-title>Habits</overview-title>
+          </overview-column>
+          <overview-column
+            v-for="(day, col) in getLastNDays(FIVE_DAYS)"
+            :key="`habit-col-${col}`"
+          >
+            <overview-title>{{ day | formatDay }}</overview-title>
+          </overview-column>
+        </overview-row>
+        <overview-row v-for="habit in habits" :key="`habit-${habit.id}`">
+          <overview-column>
+            <router-link :to="{ name: 'habit', params: { habitId: habit.id } }">
+              {{ habit.title }}
+            </router-link>
+          </overview-column>
+          <overview-column
+            v-for="(day, col) in getLastNDays(FIVE_DAYS)"
+            :key="`habit-col-${col}`"
+          >
+            <div v-if="isHabitMark(habit.id, formatDate(day))">x</div>
+          </overview-column>
+        </overview-row>
+      </overview>
+      <add-habbit-button @click.prevent.native="openModal()"
+        >+</add-habbit-button
+      >
+    </main>
+  </div>
 </template>
 
 <script>
@@ -41,7 +46,7 @@ import Overview, {
   OverviewTitle,
 } from '@/components/Overview'
 import AddHabbitButton from '@/components/AddHabbitButton'
-
+import Sidebar from '@/components/Sidebar'
 // const HabitTitle = styled.a`
 //   font-size: 14px;
 //   color: palevioletred;
@@ -57,6 +62,7 @@ export default {
     'overview-column': OverviewColumn,
     'overview-title': OverviewTitle,
     'add-habbit-button': AddHabbitButton,
+    Sidebar,
     // 'habit-title': HabitTitle,
   },
   computed: {
@@ -94,4 +100,11 @@ export default {
 }
 </script>
 
-<style lang="css" scoped></style>
+<style lang="css">
+.bm-burger-button {
+  left: 10px;
+  top: 10px;
+  width: 24px;
+  height: 24px;
+}
+</style>
