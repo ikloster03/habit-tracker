8d1dd4928f465b01c4b665f7f840312f6c1a9e81
Ivan Monastyrev <ikloster@yandex.ru>
2020-02-14 00:25:08 +0300

    login: add facebook

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,6 +4,9 @@
     <div>
       <a href="" @click.prevent="googleLogin()">Sign In with Google</a>
     </div>
+    <div>
+      <a href="" @click.prevent="facebookLogin()">Sign In with Facebook</a>
+    </div>
   </div>
 </template>
 
@@ -17,6 +20,30 @@ export default {
     ...mapActions(['setCurrentUser']),
     googleLogin() {
       const provider = new firebase.auth.GoogleAuthProvider()
+
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then(() => {
+          const currentUser = firebase.auth().currentUser
+
+          if (currentUser) {
+            this.setCurrentUser({
+              user: {
+                name: currentUser.displayName,
+                email: currentUser.email,
+                image: currentUser.photoURL,
+              },
+            })
+          }
+          this.$router.replace('/')
+        })
+        .catch(err => {
+          alert('Oops, ', +err.message)
+        })
+    },
+    facebookLogin() {
+      const provider = new firebase.auth.FacebookAuthProvider()
 
       firebase
         .auth()
