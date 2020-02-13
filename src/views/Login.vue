06d61746c5e48d0d6929e84c35d843ab29b66135
Ivan Monastyrev <ikloster@yandex.ru>
2020-02-13 23:25:30 +0300

    views: add login

---
+++ src/views/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h3>Sign In</h3>
+    <div>
+      <a href="" @click.prevent="googleLogin()">Sign In with Google</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+
+export default {
+  name: 'Login',
+  methods: {
+    googleLogin() {
+      const provider = new firebase.auth.GoogleAuthProvider()
+
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then((data) => {
+          console.log('data', data)
+          // this.$router.replace('home')
+        })
+        .catch(err => {
+          alert('Oops, ', +err.message)
+        })
+    },
+  },
+}
+</script>
+
+<style scoped></style>
