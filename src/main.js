e5605d8d9211e92838e189a03e6b4809b10305b5
Ivan Monastyrev <ikloster@yandex.ru>
2020-02-13 23:25:46 +0300

    app: add auth

--- src/main.js
+++ src/main.js
@@ -2,15 +2,34 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as firebase from 'firebase'
 import VModal from 'vue-js-modal'
 Vue.use(VModal)
 // import { rtdbPlugin } from 'vuefire'
 
 // Vue.use(rtdbPlugin)
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN_URL,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+}
+
+firebase.initializeApp(firebaseConfig)
+
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App),
-}).$mount('#app')
+let app = null
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App),
+    }).$mount('#app')
+  }
+})
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,37 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import firebase from 'firebase'
 import Overview from '../views/Overview.vue'
 import Habit from '../views/Habit.vue'
+import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '*',
+    redirect: '/login',
+  },
+  {
     path: '/',
     name: 'overview',
     component: Overview,
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
   },
   {
     path: '/habit/:habitId',
     name: 'habit',
     component: Habit,
+    meta: {
+      requiresAuth: true,
+    },
   },
 ]
 
@@ -22,6 +39,19 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+})
+
+router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  console.log(currentUser, requiresAuth)
+  if (requiresAuth && !currentUser) {
+    next('/login')
+  } else if (!requiresAuth && currentUser) {
+    next('/')
+  } else {
+    next()
+  }
 })
 
 export default router
