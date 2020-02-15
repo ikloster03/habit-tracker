0e3639761ca676774c7fa9abe149fc2682326764
Ivan Monastyrev <ikloster@yandex.ru>
2020-02-16 00:14:07 +0300

    config: refactor firebase

--- src/main.js
+++ src/main.js
@@ -2,29 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import * as firebase from 'firebase'
+
+import { auth } from './fb-config'
+
 import VModal from 'vue-js-modal'
 Vue.use(VModal)
-// import { rtdbPlugin } from 'vuefire'
-
-// Vue.use(rtdbPlugin)
-const firebaseConfig = {
-  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
-  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN_URL,
-  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
-}
-
-firebase.initializeApp(firebaseConfig)
 
 Vue.config.productionTip = false
 
 let app = null
-
-firebase.auth().onAuthStateChanged(() => {
+auth.onAuthStateChanged(() => {
   if (!app) {
     app = new Vue({
       router,
